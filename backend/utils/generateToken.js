5fceebbe468b2f525786dfd4acad7ea09ef1c109
dipendrasingh100 <dipsingh9950@gmail.com>
2023-10-04 11:14:40 +0530

    token expires time increased

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,16 +1,15 @@
 const jwt = require("jsonwebtoken")
 
 const generateToken = (user) => {
-    const token = jwt.sign(
-        {
-            userid: user.id,
-            email: user.email,
-            aoi: user.aoi
-        },
-        process.env.SECRET_KEY,
-        { expiresIn: "2m" }
-    )
-    return token
+    return jwt.sign(
+            {
+                userid: user.id,
+                email: user.email,
+                aoi: user.aoi
+            },
+            process.env.SECRET_KEY,
+            { expiresIn: "30m" }
+        );
 }
 
 module.exports = generateToken
