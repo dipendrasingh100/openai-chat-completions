b0c86f9f5d86ec017246c0659cb19f49dc867f64
DIPENDRA SINGH <dipsingh9950@gmail.com>
2023-10-04 13:43:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ require("dotenv").config()
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({origin:"*"}))
 
 const dbUrl = "mongodb+srv://dipendrasingh:Dipendra1234@cluster0.nw3urnu.mongodb.net/?retryWrites=true&w=majority"
 
